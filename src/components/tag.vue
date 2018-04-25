174090a8a66934e8244948de14b9dae36abc5cc2
zhangyy17 <13868076924@163.com>
2018-04-25 20:07:11 +0800

    Create tag.vue

---
+++ src/components/tag.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="$style.component" v-if="visiable">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Tag',
+  props: {
+    title: String
+  },
+  data() {
+    return {
+      visiable: false
+    };
+  },
+  computed: {
+    componentClasses() {
+      return [this.$style.component];
+    }
+  }
+};
+</script>
+<style lang="postcss" module>
+.component {
+}
+</style>
