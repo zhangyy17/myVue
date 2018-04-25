a36f06892e32b080f2c0475dea8e62721278a5ba
zhangyy17 <13868076924@163.com>
2018-04-25 20:04:48 +0800

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Button from './button';
+import Checkbox from './checkbox';
+import CheckboxGroup from './checkbox.group';
+import Modal from './modal';// 该js文件统一处理所有定义的组件，输出新的组件名
+import Searchor from './searchor';
+import Selector from './selector';
+import Radio from './radio';
+import RadioGroup from './radio.group';
+import Table from './table';
+import Tag from './tag';
+import Tags from './tags';
+
+export {
+  Button,
+  Checkbox,
+  CheckboxGroup,
+  Modal,
+  Searchor,
+  Selector,
+  Radio,
+  RadioGroup,
+  Table,
+  Tag,
+  Tags
+};
